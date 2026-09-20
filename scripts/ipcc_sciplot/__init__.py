cd586b3bfad0dd5c5697069343efc7013ff114bc
GeoGeek <huazaiheng7@gmail.com>
2026-09-20 12:17:52 +0800

    feat: export structured audit API

--- scripts/ipcc_sciplot/__init__.py
+++ scripts/ipcc_sciplot/__init__.py
@@ -3,7 +3,7 @@
 from importlib.metadata import PackageNotFoundError, version
 
 from .colormaps import load_ipcc_colormap, official_colormap_root
-from .fidelity import audit_figure
+from .fidelity import AuditCheck, AuditReport, audit_figure, audit_figure_report
 from .provenance import build_provenance, sha256_file, write_provenance
 from .recipe import FigureRecipe, load_recipe
 from .style import (
@@ -33,14 +33,14 @@ except PackageNotFoundError:
     __version__ = "0+unknown"
 
 __all__ = [
-    "AR6_REPORT_RCP",
+    "AR6_REPORT_RCP",\n    "AuditCheck",\n    "AuditReport",
     "AR6_REPORT_SSP",
     "FigureRecipe",
     "GENERIC_LINE_COLORS",
     "GENERIC_SHADE_COLORS",
     "WGI_GUIDE_2022_SSP",
     "__version__",
-    "audit_figure",
+    "audit_figure",\n    "audit_figure_report",
     "audit_text_conventions",
     "axis_label",
     "build_provenance",
